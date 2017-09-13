50b0bc190f02a9a42cd2025d148fbc3fd56317ef
Hameed Gifford <giff.h92@gmail.com>
2017-09-13 14:57:12 -0500

    Wrote serializer classes

--- cahgen/models.py
+++ cahgen/models.py
@@ -1,25 +1,26 @@
 from re import IGNORECASE
 from uuid import uuid4
 
-from django.core.validators import MinLengthValidator, RegexValidator
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class PackProfile(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
     value = models.CharField(max_length=6, validators=[RegexValidator(regex=r'[0-9a-f]{6}', flags=IGNORECASE)])
     color_name = models.CharField(max_length=30, null=True)
-    private = models.BooleanField(default=False)
 
     def __str__(self):
         return f'#{self.value} {self.color_name}'
 
     class Meta:
-        ordering = ('value',)
+        ordering = ('created',)
 
 
 class CardsList(models.Model):
-    name = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
+    cards = models.TextField(default="Example card")
     profile = models.ForeignKey(PackProfile, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
@@ -30,19 +31,25 @@ class CardsList(models.Model):
 
 
 class RenderSpec(models.Model):
-    name = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=50)
     packs = models.ManyToManyField(CardsList)
 
     def __str__(self):
         return f'{self.name}: {self.packs}'
 
+    class Meta:
+        ordering = ('created',)
+
 
 class PDF(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name='ID')
     created = models.DateTimeField(auto_now_add=True)
-    render_spec = models.OneToOneField(RenderSpec, null=True, on_delete=models.SET_NULL)
-    pdf = models.BinaryField(null=True)
+    pdf = models.BinaryField()
+    render_spec = models.ForeignKey(RenderSpec, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return str(self.render_spec)
+
+    class Meta:
+        ordering = ('created',)
--- cahgen/serializers.py
+++ cahgen/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from .models import CardsList, PackProfile, PDF, RenderSpec
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -12,3 +14,80 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+
+class PackProfileSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created = serializers.DateTimeField(read_only=True)
+    value = serializers.CharField(max_length=6)  # FIXME validate hex
+    color_name = serializers.CharField(max_length=30, required=False)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `PackProfile` instance, given the validated data.
+        """
+        return PackProfile.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `PackProfile` instance, given the validated data.
+        """
+        instance.value = validated_data.get('value', instance.value)
+        instance.color_name = validated_data.get('color_name', instance.color_name)
+
+
+class CardsListSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField(max_length=50)
+    created = serializers.DateTimeField(read_only=True)
+    # profile = serializers.ForeignKey(PackProfile, null=True, on_delete=models.SET_NULL)
+    cards = serializers.CharField()
+
+    def create(self, validated_data):
+        """
+        Create and return a new `CardsList` instance, given the validated data.
+        """
+        return CardsList.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `CardsList` instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.cards = validated_data.get('cards', instance.cards)
+
+
+class RenderSpecSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created = serializers.DateTimeField(read_only=True)
+    name = serializers.CharField(max_length=50)
+
+    def create(self, validated_data):
+        """
+        Create and return a new `RenderSpec` instance, given the validated data.
+        """
+        return RenderSpec.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `RenderSpec` instance, given the validated data.
+        """
+        instance.name = validated_data.get('name', instance.name)
+
+
+class PDFSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    created = serializers.DateTimeField(read_only=True)
+    # pdf = serializers.FileField()  # TODO find custom BinaryField
+
+    def create(self, validated_data):
+        """
+        Create and return a new `RenderSpec` instance, given the validated data.
+        """
+        return RenderSpec.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing `RenderSpec` instance, given the validated data.
+        """
+        pass
