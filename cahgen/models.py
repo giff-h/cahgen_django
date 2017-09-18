e96d339651181ca79d1a4bcc5f9633e3c946f6d1
Hameed Gifford <giff.h92@gmail.com>
2017-09-17 19:22:17 -0500

    Factored some model fields, renamed and changed 2 PDF fields, added owner to all models

--- cahgen/models.py
+++ cahgen/models.py
@@ -5,8 +5,14 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 
-class PackProfile(models.Model):
+class BaseModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class PackProfile(BaseModel):
     value = models.CharField(max_length=6, validators=[RegexValidator(regex=r'[0-9a-f]{6}', flags=IGNORECASE)])
     color_name = models.CharField(max_length=30, null=True)
     owner = models.ForeignKey('auth.User', related_name='packprofiles', on_delete=models.CASCADE)
@@ -15,30 +21,30 @@ class PackProfile(models.Model):
         return f'#{self.value} {self.color_name}'
 
 
-class CardsList(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+class CardsList(BaseModel):
     name = models.CharField(max_length=50)
     cards = models.TextField(default="Example card")
     profile = models.ForeignKey(PackProfile, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey('auth.User', related_name='cardslists', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class RenderSpec(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
+class RenderSpec(BaseModel):
     name = models.CharField(max_length=50)
     packs = models.ManyToManyField(CardsList)
+    owner = models.ForeignKey('auth.User', related_name='renderspecs', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name} :: {self.packs.all()}'
 
 
-class PDF(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name='ID')
-    created = models.DateTimeField(auto_now_add=True)
-    pdf = models.BinaryField()
+class PDF(BaseModel):
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True)
+    generated_content = models.BinaryField()
     render_spec = models.ForeignKey(RenderSpec, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey('auth.User', related_name='pdfs', on_delete=models.CASCADE)
 
     def __str__(self):
         return str(self.render_spec)
